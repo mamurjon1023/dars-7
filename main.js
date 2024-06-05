c6b364fd5134bb449064d90be49112e741257e16
mamurjon1148 <mamurjonovmamurjon01@gmail.com>
2024-06-05 19:08:50 +0500

    done

---
+++ main.js
@@ -0,0 +1,7 @@
+let array = ["Olma", "Uzum", "Nok", "Behi"];
+console.log(array.length);
+alert(`Arrayning uzunligi ${array.length} ta`);
+confirm("Tasdiqlang");
+array.shift();
+console.log(array.length);
+alert(`Arrayning uzunligi ${array.length} ta`);
